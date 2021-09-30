25843c66cc0f9def2a7704820099799ca9921eda
takamk2 <y.m.t.m.h.h.s@gmail.com>
2021-09-30 16:35:50 +0900

    TASK-5 OAuth認証

--- resources/js/containers/DashboardContainer.vue
+++ resources/js/containers/DashboardContainer.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="container">
+        <div><a href="/auth/google/redirect">Googleログイン</a></div>
+        <div><a href="/logout">ログアウト</a></div>
         <ul>
             <li v-for="bookmark in bookmarks" :key="bookmark.id"><a :href="bookmark.url" target="_blank">{{ bookmark.title }}</a></li>
         </ul>
